7f8d74d2c3500b76bae3738fb829d589901bf8bc
Alex Stocks <stocksalex@icloud.com>
2020-08-11 09:34:00 +0100

    Final Push

--- Scanner.py
+++ Scanner.py
@@ -10,28 +10,28 @@ import time
 # can make iterators through each packet in a network and send
 from netaddr import IPNetwork, IPAddress
 
-host = "192.168.0.1"
+hostIP = "192.168.0.1"
 
-subnet = "192.168.0.0/24"
+subnetIP = "192.168.0.0/24"
 
 # string check ICMP responses for
 checkICMP = "JAVAISTHEBEST"
 
 
-def udp_sender(sender, checkICMP):
+def sendUDP(senderSock, checkICMP):
     time.sleep(5)
-    sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    senderSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    for ip in IPNetwork(subnet):
+    for ip in IPNetwork(subnetIP):
         try:
-            # sends udp datagrams ar each ip in subnet
-            sender.sendto(checkICMP, ("%s" % ip, 65212))
+            # sends udp datagrams ar each ip in subnetIP
+            senderSock.sendto(checkICMP, ("%s" % ip, 65212))
         except TimeoutError:
             pass
 
 
-class IP(Structure):
-    # maps the first 20 bytes of recieved buffer into an IP header
+class IPStruct(Structure):
+    # maps the first 20 bytes of received buffer into an IP header
     _fields_ = [
         ("ihl", c_ubyte, 4),  # specifies field is 4 bits wide
         ("version", c_ubyte, 4),  # specifies field is 4 bits wide
@@ -51,62 +51,59 @@ class IP(Structure):
 
     def __init__(self, socket_buffer=None):
         # sets up the output to be readable
-        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+        self.protocolMap = {1: "ICMP", 6: "TCP", 17: "UDP"}
 
-        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
-        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+        self.srcAddress = socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dstAddress = socket.inet_ntoa(struct.pack("<L", self.dst))
 
         try:
-            self.protocol = self.protocol_map[self.protocol_num]
+            self.protocol = self.protocolMap[self.protocol_num]
         except IndexError:
             self.protocol = str(self.protocol_num)
 
 
 # Same as in original sniffer
 if os.name == "nt":
-    sockProtocol = socket.IPPROTO_IP
+    protocolSock = socket.IPPROTO_IP
 else:
-    sockProtocol = socket.IPPROTO_ICMP
+    protocolSock = socket.IPPROTO_ICMP
 
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, sockProtocol)
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, protocolSock)
 
-sniffer.bind(host, 0)
+sniffer.bind(hostIP, 0)
 sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
 if os.name == "nt":
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-t = threading.Thread(target=udp_sender, args=(subnet, checkICMP))
-t.start()
+thread = threading.Thread(target=sendUDP, args=(subnetIP, checkICMP))
+thread.start()
 
 try:
     # when loop finished, have got an ICMP packet
     while True:
-        raw_buffer = sniffer.recvfrom(65565)[0]
+        rawBuffer = sniffer.recvfrom(65565)[0]
 
-        ip_header = IP(raw_buffer[0:20])
+        ipHeader = IPStruct(rawBuffer[0:20])
 
-        print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+        print("Protocol: %s %s -> %s" % (ipHeader.protocol, ipHeader.srcAddress, ipHeader.dstAddress))
 
-        if ip_header.protocol == "ICMP":
+        if ipHeader.protocol == "ICMP":
             # get head of ICMP packet
             # ihl says number of 32-bit words in the ip header so 4 * ihl is size of header
             # where this ends we know ICMP starts
-            offset = ip_header.ihl * 4
+            offset = ipHeader.ihl * 4
 
-            buf = raw_buffer[offset:offset + sizeof(ICMPStruct)]
+            buffer = rawBuffer[offset:offset + sizeof(ICMPStruct)]
 
-            icmp_header = ICMPStruct(buf)
+            icmpHeader = ICMPStruct(buffer)
 
-            print("ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code))
+            print("ICMP -> Type: %d Code: %d" % (icmpHeader.type, icmpHeader.code))
 
-            if icmp_header.code == 3 and icmp_header.type == 3:
-                if IPAddress(ip_header.src_address) in IPNetwork(subnet):
-                    if raw_buffer[len(raw_buffer) - len(checkICMP):] == checkICMP:
-                        print("Host Up: %s" % ip_header.src_address)
-
-
-
+            if icmpHeader.code == 3 and icmpHeader.type == 3:
+                if IPAddress(ipHeader.srcAddress) in IPNetwork(subnetIP):
+                    if rawBuffer[len(rawBuffer) - len(checkICMP):] == checkICMP:
+                        print("hostIP Up: %s" % ipHeader.srcAddress)
 except KeyboardInterrupt:
     if os.name == "nt":
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
